06bffdb6c679b9400865d8f4b79131bee2b69a18
RoFhdsa <151877297+RoFhdsa@users.noreply.github.com>
2024-03-02 03:08:30 +0400

    finish

--- test/test_buy_products.py
+++ test/test_buy_products.py
@@ -31,19 +31,19 @@ class TestProductPage:
             print(f'result_test = {result_test.__dict__}')
             assert  expected_result_test == result_test
 
-        # with allure.step(f"Проверка формы заказача на сайте Mozila"):
-        #     result_test = main_page_ff.buy_products(type_program=data_case.type_program,
-        #                                          period_use_program=data_case.period_use_program,
-        #                                          is_weekend=data_case.is_weekend,
-        #                                          is_coupon=data_case.is_coupon, coupon=data_case.coupon,
-        #                                          is_number_phone=data_case.is_number_phone,
-        #                                          number_phone=data_case.number_phone,
-        #                                          is_friend=data_case.is_friend,
-        #                                          number_phone_friend=data_case.number_phone_friend,
-        #                                          expected_page=data_case.expected_page)
-        #     print(f'expected_result_test = {expected_result_test.__dict__}')
-        #     print(f'result_test = {result_test.__dict__}')
-        #     assert  expected_result_test == result_test
+        with allure.step(f"Проверка формы заказача на сайте Mozila"):
+            result_test = main_page_ff.buy_products(type_program=data_case.type_program,
+                                                 period_use_program=data_case.period_use_program,
+                                                 is_weekend=data_case.is_weekend,
+                                                 is_coupon=data_case.is_coupon, coupon=data_case.coupon,
+                                                 is_number_phone=data_case.is_number_phone,
+                                                 number_phone=data_case.number_phone,
+                                                 is_friend=data_case.is_friend,
+                                                 number_phone_friend=data_case.number_phone_friend,
+                                                 expected_page=data_case.expected_page)
+            print(f'expected_result_test = {expected_result_test.__dict__}')
+            print(f'result_test = {result_test.__dict__}')
+            assert  expected_result_test == result_test
 
         allure.dynamic.severity(allure.severity_level.CRITICAL)
         allure.link("https://tracker.yandex.ru/TLB-1", name="Задача:    TLB-1: Проверка формы Заказа")
